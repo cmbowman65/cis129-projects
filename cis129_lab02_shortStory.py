ba31ff15d6f806c48125c87ea0c2d5d79863b962
cmbowman65 <cmbowman@mail.pima.edu>
2024-06-04 13:32:30 -0700

    Replaced "Hello World" with a short story for cis129_lab02_shortStory.py

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,8 @@
-print('Hello, World!')
+# Author: Cliff Bowman
+# Lab for Module 2 in my CIS129 course
+# Prints a simple short story
+print('I graduated from the University of Arizona in 1988.')
+print('I served in the US Air Force for 21 years and flew the A-10 Warthog.')
+print('I continue to train new A-10 pilots as a civilian contractor.')
+print('My wife, Kathleen, and I have raised four awesome children, now adults.')
+print('We have replaced our kids with two cats, one turtle, and 14 chickens.')
