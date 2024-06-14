d3b26c67087c062ced994f17a1bdb130ba7ae529
cmbowman65 <cmbowman@mail.pima.edu>
2024-06-13 21:58:22 -0700

    Update cis129_lab03_coffeeShop.py

--- cis129_lab03_coffeeShop.py
+++ cis129_lab03_coffeeShop.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 # cis129_lab03_coffeeShop.py
 
 """
@@ -7,24 +5,31 @@ Created on Thu Jun 13 15:17:34 2024
 
 @author: Cliff Bowman
 
-Provide a receipt for coffee and muffins purchased
+Provide a receipt for items purchased
 """
 
 coffee = int(input('Enter the number of coffees purchased:  '))
 muffin = int(input('Enter the number of muffins purchased:  '))
+bagel = int(input('Enter the number of bagels purchased:  '))
+danish = int(input('Enter the number of danishes purchased:  '))
 
 coffee_tot = coffee * 5.00
 muffin_tot = muffin * 4.00
-tax = ((coffee_tot + muffin_tot) * 6) / 100
-total = (((tax + coffee_tot + muffin_tot)  * 100) // 1) / 100
+bagel_tot = bagel * 2.00
+danish_tot = danish * 2.50
+tax = ((coffee_tot + muffin_tot + bagel_tot + danish_tot) * 6) / 100
+total = (((tax + coffee_tot + muffin_tot + bagel_tot + danish_tot)  * 100) // 1) / 100
 
 print('\n') #blank line between input and results
 
 print('****************************************\
-      \nMy Coffee and Muffin Shop\
+      \nK & C Coffee and Muffin Shop\
       \nNumber of coffees bought?\n', coffee)
 
-print('Number of muffins bought?\n', muffin, '\
+print('Number of muffins bought?\n', muffin
+print('Number of bagels bought?\n', bagel
+
+print('Number of danishes bought?\n', danish, '\
       \n****************************************')
 
 print('\n\n')
@@ -42,10 +47,25 @@ if muffin == 1:
     print (muffin, 'Muffin at $4 each:  $', muffin_tot)    
 if muffin > 1:
     print(muffin, 'Muffins at $4 each:  $', muffin_tot) #make muffins plural
+
+"""printing "bagel" or "bagels" on the receipt"""
+if bagel == 1:
+    print(bagel, 'Bagel at $2 each:  $', bagel_tot)  
+if bagel > 1:
+    print(bagel, 'Bagels at $2 each:  $', bagel_tot) #make bagels plural
+
+"""printing "danish" or "danishes" on the receipt"""
+if danish == 1:
+    print (danish, 'Danish at $2.50 each:  $', danish_tot)    
+if danish > 1:
+    print(danish, 'Danishes at $2.50 each:  $', danish_tot) #make danishes plural
     
 print('6% tax:  $', tax)
 print('---------')
 print('Total:  $', total, '\
       \n*****************************************')
+
+print('\nWe greatly appreciate your business.')
+print('\n        Come Back Soon!')
       
       
