afc5e1d9fbdb8710d92c73a47a424e097e3a8b21
cmbowman65 <cmbowman@mail.pima.edu>
2024-06-16 08:59:54 -0700

    Update CIS129_CliffBowman_Lab4.py

--- CIS129_CliffBowman_Lab4.py
+++ CIS129_CliffBowman_Lab4.py
@@ -3,43 +3,45 @@
 # 15 Jun 2024
 # This program determines the store and individual bonus amounts
 
-# declare local variables
+# Variables for sales and percentage amounts
 monthlySales = 0  # monthly sales amount
 storeAmount = 0  # store bonus amount
 empAmount = 0  # employee bonus amount
 salesIncrease = 0  # percent of sales increase
 
 # This code gets the monthly sales
-monthlySales = float(input('Enter the monthly sales:  '))
+monthlySales = float(input('Enter the monthly sales: '))
 
-# This code determines the storeAmount bonus
+# This code determines the store bonus
 if monthlySales >= 110000:
     storeAmount = 6000
 elif monthlySales >= 100000:
-	storeAmount = 5000
+    storeAmount = 5000
 elif monthlySales >= 90000:
-	storeAmount = 4000
+    storeAmount = 4000
 elif monthlySales >= 80000:
-	storeAmount = 3000
+    storeAmount = 3000
 else:
-	storeAmount = 0
+    storeAmount = 0
 	
-# This code gets the percent of increase in sales
+# This code determines the percent of increase in sales
 salesIncrease = float(input('Enter percent of sales increase:  '))
 salesIncrease = salesIncrease / 100
 
-# This code determines the empAmount bonus
+# This code determines the employee bonus
 if salesIncrease >= .05:
-	empAmount = 75
+    empAmount = 75
 elif salesIncrease >= .04:
-	empAmount = 50
+    empAmount = 50
 elif salesIncrease >= .03:
-	empAmount = 40
+    empAmount = 40
 else: 
-	empAmount = 0
+    empAmount = 0
 
-# This code prints the bonus information
+# This code prints the store and employee bonus information
 print("The store bonus amount is $", storeAmount)
 print("The employee bonus amount is $", empAmount)
+
+# This code prints a statement if both maximum bonuses are met 
 if (storeAmount == 6000 ) and (empAmount == 75):
     print('Congrats! You have reached the highest bonus amounts possible! ')
