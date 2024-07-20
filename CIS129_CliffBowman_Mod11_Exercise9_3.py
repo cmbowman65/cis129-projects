ab3eeb6a3a2f5c15941f877eecd64614428366b2
cmbowman65 <cmbowman@mail.pima.edu>
2024-07-19 18:48:13 -0700

    Update CIS129_CliffBowman_Mod11_Exercise9_3.py

--- CIS129_CliffBowman_Mod11_Exercise9_3.py
+++ CIS129_CliffBowman_Mod11_Exercise9_3.py
@@ -5,45 +5,43 @@ Cliff Bowman
 CIS-129
 19 Jul 2024
 
-This program has a user input an unlimited number of students and grades  
-and stores them in a csv file named grades.csv.
+This program has the user input an unlimited number of students and grades  
+and stores the data in a csv file named grades.csv.
 
 """
 
 import csv
 
 
-def main():
+def main():      
     
     # creates and opens csv file
     with open('grades.csv', 'w', newline='') as grades:
         writer = csv.writer(grades)
-       
+        
+        # sets initial condition prior to loop
+        firstname = input("Student's first name or 'end' if complete: ")   
+    
+        # check for only letters in user input
+        while firstname.isalpha() == False:
+            firstname = input("Only letters please.  Student's first name or 'end' if complete: ")
+    
         # loop to collect data until sentinel entered
-        while True:
-            firstname = input("Student's first name or 'end' if complete: ")
-            
-            # check for letters only in firstname
-            while firstname.isalpha() == False:
-                firstname = input("Only letters please.  Student's first name or 'end' if complete: ")
-            
-            # check for sentinel to end the program
-            if firstname.lower() == 'end':
-                break
-            
+        while firstname.lower() != 'end':  
+                        
             # store first name with first letter in caps
             firstname = firstname.capitalize()
-            
+                
             lastname = input("Student's last name: ")
-            
-            # check for only letters in lastname
+                
+            # check for only letters in user input
             while lastname.isalpha() == False:
                 lastname = input("Only letters please.  Student's last name: ")
-            
+                
             # store last name with first letter in caps
             lastname = lastname.capitalize()
-          
-            # enter exam grades and call functions to check for valid integers
+              
+            # enter exam grades and calls function to check for valid integers
             exam1grade = goodGrade("First exam grade: ")
             exam2grade = goodGrade("Second exam grade: ")
             exam3grade = goodGrade("Third exam grade: ")
@@ -51,13 +49,19 @@ def main():
             # write data to the csv file
             writer.writerow([firstname, lastname, exam1grade, exam2grade, exam3grade])
             
-            print()
+            # start a new input or enter sentinel
+            firstname = input("\nStudent's first name or 'end' if complete: ")   
+            
+            # check for only letters in the user input
+            while firstname.isalpha() == False:
+                firstname = input("Only letters please.  Student's first name or 'end' if complete: ")
+
 
 # Ensures valid user input for exam grades of a positive integer or zero
 def goodGrade(user_input):
     good_input = validNumber(user_input)
     while good_input < 0 or good_input > 100:
-        print('Enter a integer between zero and 100 for a grade. Try again.')
+        print('Enter an integer between zero and 100 for a grade. Try again.')
         good_input = validNumber(user_input)
     return good_input
 
@@ -68,7 +72,7 @@ def validNumber(user_input):
             num = int(input(user_input))
             return num
         except ValueError:
-            print('Enter a number greater than or equal to 0. No letters or decimals allowed. Please try again.')  
+            print('Enter a number between zero and 100. No letters or decimals allowed. Please try again.')  
 
 main()
 
